6bab5d16bdd5f86ee413376e751a5d306fd8a457
Douglas Taquary <douglastaquary@gmail.com>
2021-03-17 22:42:24 -0300

    Setup androidapp dependecies.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -19,9 +19,62 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-beta02"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
+            freeCompilerArgs = listOf("-Xallow-jvm-ir-dependencies", "-Xskip-prerelease-check",
+                "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.0")
+
+    implementation("androidx.compose.ui:ui:1.0.0-beta02")
+    implementation("androidx.compose.ui:ui-graphics:1.0.0-beta02")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta02")
+    implementation( "androidx.compose.foundation:foundation-layout:1.0.0-beta02")
+    implementation("androidx.compose.material:material:1.0.0-beta02")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta02")
+    implementation("androidx.navigation:navigation-compose:1.0.0-alpha09")
+    implementation("dev.chrisbanes.accompanist:accompanist-coil:0.6.2")
+
+    implementation("io.insert-koin:koin-core:3.0.1-beta-1")
+    implementation("io.insert-koin:koin-android:3.0.1-beta-1")
+    implementation("io.insert-koin:koin-androidx-compose:3.0.1-beta-1")
+
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.test:runner:1.2.0")
+
+    implementation(project(":shared"))
 }
