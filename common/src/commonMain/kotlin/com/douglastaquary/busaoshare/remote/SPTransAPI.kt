ac2d32758eacaee360428bb801d7059742954a4a
Diego1906 <di_fsantos@yahoo.com.br>
2022-10-19 14:04:56 -0300

    Adicionar uma variável para armazenar o cookie da Api que é retornado na autenticação e que será enviado nas próximas requisições

--- common/src/commonMain/kotlin/com/douglastaquary/busaoshare/remote/SPTransAPI.kt
+++ common/src/commonMain/kotlin/com/douglastaquary/busaoshare/remote/SPTransAPI.kt
@@ -2,41 +2,40 @@ package com.douglastaquary.busaoshare.remote
 
 import co.touchlab.kermit.Logger
 import com.douglastaquary.busaoshare.model.ArrivalOfVehiclesPerTrip
-import com.douglastaquary.busaoshare.model.Result
 import com.douglastaquary.busaoshare.model.Trip
 import io.ktor.client.HttpClient
 import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.request.request
-import io.ktor.client.statement.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 
 class SPTransAPI(
     private val client: HttpClient,
-    val baseUrl: String = "http://api.olhovivo.sptrans.com.br/v2.1"
+    private val baseUrl: String = "http://api.olhovivo.sptrans.com.br/v2.1"
 ) {
-    private val nonStrictJson = Json { isLenient = true; ignoreUnknownKeys = true }
+    private var cookie: String? = null
 
     suspend fun authentication(): Boolean {
         return client.post {
             url("$baseUrl/Login/Autenticar")
-            parameter("token", Companion.API_TOKEN)
+            parameter("token", API_TOKEN)
+        }.also {
+            cookie = it.headers["set-cookie"]
         }.body()
     }
 
     suspend fun fetchTrips(searchText: String): List<Trip> {
         return client.get {
             url("$baseUrl/Linha/Buscar")
-            parameter("termosBusca", "$searchText")
-            Logger.a("Requesting fetchTrips - params: $searchText ")
+            parameter("termosBusca", searchText)
+            header("Cookie", cookie)
+            Logger.a("Requesting fetchTrips - params: $searchText")
         }.body()
     }
 
     suspend fun fetchTripArrives(tripNumber: Int): List<ArrivalOfVehiclesPerTrip> {
         return client.get {
             url("$baseUrl/Previsao/Linha")
-            parameter("codigoLinha", "$tripNumber")
+            parameter("codigoLinha", tripNumber)
+            header("Cookie", cookie)
         }.body()
     }
 
